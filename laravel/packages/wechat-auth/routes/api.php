4b813f606fec1157fb823aa03a995b5760f51354
echoshiki <brokenworldand.me@gmail.com>
2025-03-05 22:17:27 +0800

    重新构建

--- laravel/packages/wechat-auth/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Echoshiki\WechatAuth\Http\Controllers\LoginController;
-
-Route::prefix('wechat-auth')->group(function () {
-    Route::post('/login', [LoginController::class, 'login']);
-    Route::get('/package-info', [LoginController::class, 'packageInfo']);
-});
