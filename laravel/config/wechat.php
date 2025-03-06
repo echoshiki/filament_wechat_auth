1f4422c40d90daef44e31a394da55a7d1c0a96ca
echoshiki <brokenworldand.me@gmail.com>
2025-03-06 11:46:52 +0800

    部分配置

---
+++ laravel/config/wechat.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'mini' => [
+        'app_id' => env('WECHAT_MINI_APP_ID'),
+        'secret' => env('WECHAT_MINI_APP_SECRET'),
+        'token' => env('WECHAT_MINI_APP_TOKEN'),
+        'aes_key' => env('WECHAT_MINI_APP_AES_KEY'),
+    ],
+];
