5f45c04d8435e8dce55294923f37015b74a60659
echoshiki <brokenworldand.me@gmail.com>
2025-03-06 11:28:34 +0800

    install filament-shield

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// 引入 RBAC 权限管理
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
